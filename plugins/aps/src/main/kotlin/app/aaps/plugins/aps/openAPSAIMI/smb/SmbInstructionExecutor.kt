b786fed8e0ba580fc9570e64e3f1e5fdb2e1488f
mtr93600 <tellier@pm.me>
2025-12-04 08:54:51 +0100

    smb damping

--- plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSAIMI/smb/SmbInstructionExecutor.kt
+++ plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSAIMI/smb/SmbInstructionExecutor.kt
@@ -343,7 +343,7 @@ object SmbInstructionExecutor {
 
         val mealModeRun =
             input.mealTime || input.bfastTime || input.lunchTime || input.dinnerTime || input.highCarbTime
-        val hyperPlateauActive = (input.bg >= 160.0 && input.predictedBg.toDouble() >= 180.0) &&
+        val hyperPlateauActive = (input.bg >= 130.0 && input.predictedBg.toDouble() >= 130.0) &&
             (input.delta < 1.0 && input.combinedDelta < 3.0) &&
             (input.iob < input.maxSmb)
 
