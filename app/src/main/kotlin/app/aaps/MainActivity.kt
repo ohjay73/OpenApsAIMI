1e4f3e29ee2790bd51c96665c53d90cbf5b0feae
mtr93600 <?M@th13U!>
2026-04-07 12:05:06 +0200

    Update user property setting for active APS in MainActivity
    - Changed the user property setting for active APS to use safe call operator, ensuring it handles null values gracefully.
    - This update prevents potential crashes when active APS is not available, improving app stability.

--- app/src/main/kotlin/app/aaps/MainActivity.kt
+++ app/src/main/kotlin/app/aaps/MainActivity.kt
@@ -469,7 +469,7 @@ class MainActivity : DaggerAppCompatActivityWithResult() {
         if (hashes.isNotEmpty()) fabricPrivacy.setUserProperty("Hash", hashes[0])
         activePlugin.activePump.let { fabricPrivacy.setUserProperty("Pump", it::class.java.simpleName) }
         if (!config.AAPSCLIENT && !config.PUMPCONTROL)
-            activePlugin.activeAPS.let { fabricPrivacy.setUserProperty("Aps", it::class.java.simpleName) }
+            activePlugin.activeAPS?.let { fabricPrivacy.setUserProperty("Aps", it::class.java.simpleName) }
         activePlugin.activeBgSource.let { fabricPrivacy.setUserProperty("BgSource", it::class.java.simpleName) }
         fabricPrivacy.setUserProperty("Profile", activePlugin.activeProfileSource.javaClass.simpleName)
         activePlugin.activeSensitivity.let { fabricPrivacy.setUserProperty("Sensitivity", it::class.java.simpleName) }
